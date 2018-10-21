30f3d01dfd14564658b1592b0359000b759c4de7
abdelaziz <abdelaziz.iddouch@gmail.com>
2018-10-22 00:25:59 +0100

    set up the user  authentication

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
-const mangoose = require('mongoose');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+const promisify = require('es6-promisify');
 
 exports.loginForm = (req, res) => {
   res.render('login', { title: 'Login' });
@@ -40,4 +42,14 @@ exports.validateRegister = (req, res, next) => {
     return;
   }
   next(); // No errors
+};
+
+exports.register = async (req, res, next) => {
+  const user = new User({ email: req.body.email, name: req.body.name });
+  // User.register(user, req.body.password, function(err, user) {
+  // });
+  // make User.register promise based instead of callback based
+  const register = promisify(User.register, User);
+  await register(user, req.body.password);
+  next(); // pass to authController.login
 };
